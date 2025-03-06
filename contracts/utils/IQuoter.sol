d67bf0601e7bea895fc57aea89167fff284c24a2
francescocirulli <63744530+francescocirulli@users.noreply.github.com>
2025-03-06 12:30:23 +0100

    remove unused files

--- contracts/utils/IQuoter.sol
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-interface IQuoter {
-    function quoteExactInput(bytes memory path, uint256 amountIn) external view returns (uint256 amountOut);
-}
